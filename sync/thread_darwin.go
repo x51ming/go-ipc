a7c4b8370c8721079ee97a6f65dc0b9bdeb6d39c
nolva@mac <nolva@qq.com>
2025-01-04 11:35:12 +0800

    sync: fix thread_darwin

--- sync/thread_darwin.go
+++ sync/thread_darwin.go
@@ -2,15 +2,31 @@
 
 package sync
 
-import "golang.org/x/sys/unix"
+// #include <pthread.h>
+import "C"
+import (
+	"errors"
+	"syscall"
 
-func mach_thread_self() uint32
+	"golang.org/x/sys/unix"
+)
 
 func gettid() (uint32, error) {
-	return mach_thread_self(), nil
+	port := C.pthread_mach_thread_np(C.pthread_self())
+	if port == 0 {
+		return 0, errors.New("failed to get mach port of the current thread")
+	}
+	return uint32(port), nil
 }
 
 func killThread(port uint32) error {
-	_, _, err := unix.Syscall(unix.SYS___PTHREAD_KILL, uintptr(port), uintptr(unix.SIGUSR2), 0)
-	return err
+	thread := C.pthread_from_mach_thread_np(C.mach_port_t(port))
+	if thread == nil {
+		return errors.New("failed to get pthread_t")
+	}
+	r := C.pthread_kill(thread, C.int(syscall.SIGUSR2))
+	if r != 0 {
+		return unix.Errno(r)
+	}
+	return nil
 }
